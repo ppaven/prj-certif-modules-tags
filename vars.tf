772798512b9fa45ba78195e72df17193b1ea8639
Philippe PAVEN <philippe.paven@easyteam.fr>
2024-10-18 18:11:07 +0200

    after terraform fmt

--- vars.tf
+++ vars.tf
@@ -1,9 +1,9 @@
-variable app_code {}
-variable app_owner {}
-variable date_creation {}
-variable app_criticity {}
-variable technical_contact {}
+variable "app_code" {}
+variable "app_owner" {}
+variable "date_creation" {}
+variable "app_criticity" {}
+variable "technical_contact" {}
 
-variable auto_shutdown_schedule {
+variable "auto_shutdown_schedule" {
   default = "never"
 }
